41d710215cf9ef558e7204818161aa68b54bc31f
Patrik Švikruha <patrik.svikruha@gmail.com>
2022-03-29 09:08:59 +0200

    update active link

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
 <template>
     <header>
         <router-link to="/">View A</router-link>
-        <br>
+        <br />
         <router-link to="/viewb">View B</router-link>
-        <br>
+        <br />
         <router-link to="/todos">Todos</router-link>
         <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
     </header>
@@ -18,10 +18,15 @@
 </script>
 
 
-<style scoped>
+<style>
 .logo {
     position: absolute;
     top: 0px;
     right: 0px;
+}
+.router-link-active {
+    color: red;
+    text-decoration: none;
+    text-transform: uppercase;
 }
 </style>
