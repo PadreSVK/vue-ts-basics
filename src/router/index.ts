31e23b7f760a43d90fb4464ab4ff49f6a6d9f3f2
Patrik Švikruha <patrik.svikruha@gmail.com>
2022-03-29 12:25:00 +0200

    split ot VIte chunks

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,18 +3,28 @@
 import { createRouter, createWebHistory } from "vue-router";
 import ViewA from "@/views/ViewA.vue";
 import ViewB from "@/views/ViewB.vue";
-import Todo from "@/views/Todo.vue";
-import UserProfile from "@/views/UserProfile.vue";
+// import Todo from "@/views/Todo.vue";
+// import UserProfile from "@/views/UserProfile.vue";
 import NotFound from "@/views/404.vue";
 
+export const Routes = {
+    ViewA: "ViewA",
+    ViewB: "ViewB",
+    Todo: "Todo",
+    UserProfile: "UserProfile"
+}
 
 export const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: "/", name: "ViewA", component: ViewA },
-        { path: "/viewb/:id?", name: "ViewB", component: ViewB },
-        { path: "/todos", name: "Todo", component: Todo },
-        { path: "/profile", name: "UserProfile", component: UserProfile },
+        {
+            path: "/viewb/:id?", name: "ViewB", component: ViewB, meta: {
+                metadata: "cupakabra"
+            }
+        },
+        { path: "/todos", name: "Todo", component: import("@/views/Todo.vue") },
+        { path: "/profile", name: "UserProfile", component: import("@/views/UserProfile.vue") },
 
         { path: '/:pathMatch(.*)*', name: 'not-found', component: NotFound },
     ]
